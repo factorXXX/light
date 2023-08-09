27dcd380833f0bccb01176884b3d0a535fa54d37
factorXXX <85854212+factorXXX@users.noreply.github.com>
2023-08-10 01:20:28 +0800

    bugfix

--- script.js
+++ script.js
@@ -232,22 +232,27 @@ tmp.move=[]
         let bD=tmp.building[truemove[i][0]][truemove[i][1]]
         if(bD[1]=='up'){
           if((truemove[i][0]-1)<0){
-            tmp.building[truemove[i][0]][truemove[i][1]][1]="down";tmp.move.push([truemove[i][0],truemove[i][1]])}
+            tmp.building[truemove[i][0]][truemove[i][1]][1]="down";tmp.move.push([truemove[i][0],truemove[i][1]]);truemove[i]=['moved']}
           else if(tmp.building[truemove[i][0]-1][truemove[i][1]][0]!=null||((tmp.location[0]==truemove[i][0]-1)&&(tmp.location[1]==truemove[i][1]))){tmp.building[truemove[i][0]][truemove[i][1]][1]="down";
-            tmp.move.push([truemove[i][0],truemove[i][1]])
+            tmp.move.push([truemove[i][0],truemove[i][1]]);truemove[i]=['moved']
           }
           else {
-            tmp.building[truemove[i][0]-1][truemove[i][1]]=tmp.building[truemove[i][0]][truemove[i][1]];tmp.building[truemove[i][0]][truemove[i][1]]=[null];tmp.move.push([truemove[i][0]-1,truemove[i][1]])
+            tmp.building[truemove[i][0]-1][truemove[i][1]]=tmp.building[truemove[i][0]][truemove[i][1]];tmp.building[truemove[i][0]][truemove[i][1]]=[null]
+            tmp.move.push([truemove[i][0]-1,truemove[i][1]]);truemove[i]=['moved']
           }
         }
         else if(bD[1]=='left'){
-          if((truemove[i][1]-1)<0){tmp.building[truemove[i][0]][truemove[i][1]][1]="right";tmp.move.push([truemove[i][0],truemove[i][1]])}
-          else if(tmp.building[truemove[i][0]][truemove[i][1]-1][0]!=null||((tmp.location[0]==truemove[i][0])&&(tmp.location[1]==truemove[i][1]-1))){tmp.building[truemove[i][0]][truemove[i][1]][1]="right";tmp.move.push([truemove[i][0],truemove[i][1]])}
-          else {tmp.building[truemove[i][0]][truemove[i][1]-1]=tmp.building[truemove[i][0]][truemove[i][1]];tmp.building[truemove[i][0]][truemove[i][1]]=[null];tmp.move.push([truemove[i][0],truemove[i][1]-1])
+          if((truemove[i][1]-1)<0){
+            tmp.building[truemove[i][0]][truemove[i][1]][1]="right";tmp.move.push([truemove[i][0],truemove[i][1]]);truemove[i]=['moved']}
+          else if(tmp.building[truemove[i][0]][truemove[i][1]-1][0]!=null||((tmp.location[0]==truemove[i][0])&&(tmp.location[1]==truemove[i][1]-1))){tmp.building[truemove[i][0]][truemove[i][1]][1]="right"
+          tmp.move.push([truemove[i][0],truemove[i][1]]);truemove[i]=['moved']}
+          else {tmp.building[truemove[i][0]][truemove[i][1]-1]=tmp.building[truemove[i][0]][truemove[i][1]];tmp.building[truemove[i][0]][truemove[i][1]]=[null]
+            tmp.move.push([truemove[i][0],truemove[i][1]-1]);truemove[i]=['moved']
           }
         }
   }
   for(let i=truemove.length-1;i>=0;i--){
+    if(truemove[i][0]=='moved')continue;
     let bD=tmp.building[truemove[i][0]][truemove[i][1]]
     if(bD[1]=='down'){
       if((truemove[i][0]+1)>=tmp.area[0]){tmp.building[truemove[i][0]][truemove[i][1]][1]="up";tmp.move.push([truemove[i][0],truemove[i][1]])}
